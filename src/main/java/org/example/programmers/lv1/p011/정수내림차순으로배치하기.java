80a64b11af2e348a101a6a5104c515532214c872
BakChanGyu <qmrma987@gmail.com>
2023-07-23 00:38:50 +0900

    day02

---
+++ src/main/java/org/example/programmers/lv1/p011/정수내림차순으로배치하기.java
@@ -0,0 +1,27 @@
+package org.example.programmers.lv1.p011;
+
+import java.util.*;
+
+public class 정수내림차순으로배치하기 {
+    public long solution(long n) {
+        long answer = 0;
+
+        List<Long> digit = new ArrayList<>();
+
+        while (n > 0) {
+            digit.add(n % 10);
+            n /= 10;
+        }
+
+        Collections.sort(digit, Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < digit.size(); i++) {
+            sb.append(digit.get(i));
+        }
+
+        answer = Long.parseLong(sb.toString());
+
+        return answer;
+    }
+}
---
+++ src/main/java/org/example/programmers/lv1/p012/하샤드수.java
@@ -0,0 +1,20 @@
+package org.example.programmers.lv1.p012;
+
+public class 하샤드수 {
+    public boolean solution(int x) {
+        boolean answer = false;
+        int sum = 0;
+        int digit = x;
+
+        while (digit > 0) {
+            sum += digit % 10;
+            digit /= 10;
+        }
+
+        if (x % sum == 0) {
+            answer = true;
+        }
+
+        return answer;
+    }
+}
