9792dac75b0e9ab2612e0851ae58e509855650c1
BakChanGyu <qmrma987@gmail.com>
2023-07-23 21:55:54 +0900

    day03

---
+++ src/main/java/org/example/programmers/lv1/p014/콜라츠추측.java
@@ -0,0 +1,24 @@
+package org.example.programmers.lv1.p014;
+
+public class 콜라츠추측 {
+    public int solution(long num) {
+        int answer = 0;
+
+        while (num != 1) {
+            if (answer >= 500) {
+                answer = -1;
+                break;
+            }
+
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = num * 3 + 1;
+            }
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
---
+++ src/main/java/org/example/programmers/lv1/p016/나누어떨어지는숫자배열.java
@@ -0,0 +1,25 @@
+package org.example.programmers.lv1.p016;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class 나누어떨어지는숫자배열 {
+    public List<Integer> solution(int[] arr, int divisor) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                answer.add(arr[i]);
+            }
+        }
+
+        Collections.sort(answer);
+
+        if (answer.size() == 0) {
+            answer.add(-1);
+        }
+
+        return answer;
+    }
+}
